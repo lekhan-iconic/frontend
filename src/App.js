8964e58996c6f6726145408e81f32891e41bb62e
NARRA LEKHAN KUMAR <lekhannarra2003@gmail.com>
2023-06-12 21:07:43 +0530

    student

--- src/App.js
+++ src/App.js
@@ -6,25 +6,9 @@ import Student from "./pages/Student";
 import Login from "./pages/Login";
 
 function App() {
-  // const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const navigate = useNavigate();
-
-  // const handleLogin = () => {
-  //   // Perform login logic here
-
-  //   // Assuming successful login, set isLoggedIn to true
-  //   setIsLoggedIn(true);
-
-  //   // Navigate to the desired route
-  //   navigate("/Admin"); // Example: Navigate to the Admin page after login
-  // };
-
   return (
     <>
-      {/* <Login /> */}
       <BrowserRouter>
-        {/* <Login onLogin={handleLogin} /> */}
-
         <Routes>
           <Route path="/" element={<Login />}></Route>
           <Route path="/Home" element={<Home />}></Route>
